adc619d46915ec281b564b2c225096bb7511f32b
diegogeber <diegogeber@gmail.com>
2024-03-26 17:40:09 -0300

    file

--- src/main/java/com/event/management/controller/DemandController.java
+++ src/main/java/com/event/management/controller/DemandController.java
@@ -1,6 +1,18 @@
 package com.event.management.controller;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -16,18 +28,37 @@ public class DemandController {
 	DemandService demandService;
 	
 	
-    @PostMapping("/demand/campus-catering-full-service")
-    public String createDemand(
+    @GetMapping("/demand/campus-catering-full-service")
+    public ResponseEntity<byte[]> createDemandCCFS(
     		@RequestParam(name = "amount") int amount,
     		@RequestParam(name = "hasBuffet") boolean buffet,
     		@RequestParam(name = "mainDishes") boolean mainDishes,
     		@RequestParam(name = "deserts") boolean deserts,
+    		@RequestParam(name = "eightInchPlates") boolean eightInchPlates,
     		@RequestParam(name = "appetizers") boolean appetizers,
     		@RequestParam(name = "coldBeverage") boolean coldBeverage,
     		@RequestParam(name = "hotBeverage") boolean hotBeverage
     		
-    		) {
-        demandService.avaliateDemand(TypeDemandEnum.CCFS, amount, buffet, mainDishes, deserts, appetizers, coldBeverage, hotBeverage, false);
-        return "Demand created successfully!";
+    		) throws IOException {
+        String fileName = demandService.avaliateDemand(TypeDemandEnum.CCFS, amount, buffet, mainDishes, deserts, appetizers, coldBeverage, hotBeverage, false, eightInchPlates);
+        File file = new File(fileName);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.TEXT_PLAIN);
+        headers.setContentDispositionFormData("filename", fileName);
+        
+        if (!file.exists() || !file.isFile()) {
+            throw new FileNotFoundException("Arquivo não encontrado: " + file.getPath());
+        }
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        byte[] buffer = new byte[1024];
+        int length;
+        try (InputStream inputStream = new FileInputStream(file)) {
+            while ((length = inputStream.read(buffer)) != -1) {
+                outputStream.write(buffer, 0, length);
+            }
+        }
+        file.delete();
+        return new ResponseEntity<>(outputStream.toByteArray() , headers, HttpStatus.OK);
     }
 }
--- src/main/java/com/event/management/service/DemandService.java
+++ src/main/java/com/event/management/service/DemandService.java
@@ -1,5 +1,10 @@
 package com.event.management.service;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.UUID;
+
 import org.springframework.stereotype.Service;
 
 import com.event.management.enumerator.TypeDemandEnum;
@@ -8,58 +13,111 @@ import com.event.management.model.Order;
 @Service
 public class DemandService {
 	
-	public void avaliateDemand(TypeDemandEnum typeOrder, int amount, boolean hasBuffet,
-			boolean mainDishes, boolean deserts, boolean appetizers, boolean cold, boolean hot, boolean soda) {
+	public String avaliateDemand(TypeDemandEnum typeOrder, int amount, boolean hasBuffet,
+			boolean mainDishes, boolean deserts, boolean appetizers, boolean cold, boolean hot, boolean soda, boolean eightInchPlates) {
 		Order order = new Order(amount, typeOrder.name() , hasBuffet, mainDishes, deserts, appetizers, cold, hot, soda);
 	    
-	    ccfs(order);
+	    return ccfs(order, eightInchPlates);
 	}
 
-	private void ccfs(Order order) {
-		if("CCFS".equals(order.demand)){
-	        System.out.println("Order Demand Campus Catering Full Service");
-	    }
-
-	    if("CCFS".equals(order.demand) && order.buffet){
-	        
-	        System.out.println("Buffet Selected");
-
-	        if(order.mainDishes){
-	            System.out.println(Integer.valueOf(order.amount).toString() + " 10\" plates");
-	            System.out.println(Integer.valueOf(order.amount).toString() + " Utensil Kit");
-	            System.out.println(Integer.valueOf(order.amount).toString() + " Dinner Napkin");
-	        }
-	        
-	        if(order.deserts){
-	            System.out.println(Integer.valueOf(order.amount).toString() + " 6\" plates");
-	            System.out.println(Integer.valueOf(order.amount).toString() + " Beverage Napkin");
-	            System.out.println(Integer.valueOf(order.amount).toString() + " Forks");
-	        }
-
-	        if(order.appetizers){
-	            System.out.println(Integer.valueOf(order.amount).toString() + " 8\" plates");
-	            System.out.println(Integer.valueOf(order.amount).toString() + " Dinner Napkins");
-	            System.out.println(Integer.valueOf(order.amount).toString() + " Forks");
-	        }
-
-	    }
-
-	    if("CCFS".equals(order.demand) && (order.cold || order.hot)){
-	        
-	        System.out.println("Beverage Selected");
-
-	        if(order.cold){
-	            System.out.println(Integer.valueOf(order.amount).toString() + " Beverage Napkin");
-	            System.out.println(Integer.valueOf(order.amount).toString() + " Cold Cup");
-	            System.out.println(Integer.valueOf(order.amount).toString() + " 1 drip tray for each container");
-	        }
-	        
-	        if(order.hot){
-	            System.out.println(Integer.valueOf(order.amount).toString() + " Beverage Napkin");
-	            System.out.println(Integer.valueOf(order.amount).toString() + " Hot Cup");
-	            System.out.println(Integer.valueOf(order.amount).toString() + " Stir Cup");
-	            System.out.println(Integer.valueOf(order.amount).toString() + " 1 drip tray for each container");
-	        }
-	    }
+	private String ccfs(Order order, boolean eightInchPlates) {
+		String name = UUID.randomUUID().toString();
+		String fileName = name + ".txt";
+		try {
+			BufferedWriter  writer = new BufferedWriter(new FileWriter(fileName));
+	
+		    if("CCFS".equals(order.demand) && order.buffet){
+		        
+		        System.out.println("Buffet Selected");
+				
+			        if(order.mainDishes){
+			        	System.out.println(Integer.valueOf(order.amount).toString() + " 10\" plates");
+			            System.out.println(Integer.valueOf(order.amount).toString() + " Utensil Kit");
+			            System.out.println(Integer.valueOf(order.amount).toString() + " Dinner Napkin");
+			        	
+			        	writer.write(Integer.valueOf(order.amount).toString() + " 10\" plates");
+			        	writer.newLine();
+			            writer.write(Integer.valueOf(order.amount).toString() + " Utensil Kit");
+			            writer.newLine();
+			            writer.write(Integer.valueOf(order.amount).toString() + " Dinner Napkin");
+			            writer.newLine();
+			        }
+			        
+			        if(order.deserts){
+			        	writer.write(Integer.valueOf(order.amount).toString() + " 6\" plates");
+			        	writer.newLine();
+			        	writer.write(Integer.valueOf(order.amount).toString() + " Beverage Napkin");
+			        	writer.newLine();
+			        	writer.write(Integer.valueOf(order.amount).toString() + " Forks");
+			        	writer.newLine();
+			        	
+			            System.out.println(Integer.valueOf(order.amount).toString() + " 6\" plates");
+			            System.out.println(Integer.valueOf(order.amount).toString() + " Beverage Napkin");
+			            System.out.println(Integer.valueOf(order.amount).toString() + " Forks");
+			        }
+		
+			        if(order.appetizers){
+			        	if(eightInchPlates) {
+			        		writer.write(Integer.valueOf(order.amount).toString() + " 8\" plates");
+				        	writer.newLine();
+				        	System.out.println(Integer.valueOf(order.amount).toString() + " 8\" plates");
+			        	}
+			        	
+			        	writer.write(Integer.valueOf(order.amount).toString() + " Dinner Napkins");
+			        	writer.newLine();
+			        	writer.write(Integer.valueOf(order.amount).toString() + " Forks");
+			        	writer.newLine();
+			        	
+			            
+			            System.out.println(Integer.valueOf(order.amount).toString() + " Dinner Napkins");
+			            System.out.println(Integer.valueOf(order.amount).toString() + " Forks");
+			        }
+	
+		    }
+	
+		    if("CCFS".equals(order.demand) && (order.cold || order.hot)){
+		        
+		        System.out.println("Beverage Selected");
+	
+		        if(order.cold){
+		        	writer.write(Integer.valueOf(order.amount).toString() + " Beverage Napkin");
+		        	writer.newLine();
+		        	writer.write(Integer.valueOf(order.amount).toString() + " Cold Cup");
+		        	writer.newLine();
+		        	writer.write(Integer.valueOf(order.amount).toString() + " 1 drip tray for each container");
+		        	writer.newLine();
+		        	
+		            System.out.println(Integer.valueOf(order.amount).toString() + " Beverage Napkin");
+		            System.out.println(Integer.valueOf(order.amount).toString() + " Cold Cup");
+		            System.out.println(Integer.valueOf(order.amount).toString() + " 1 drip tray for each container");
+		        }
+		        
+		        if(order.hot){
+		        	writer.write(Integer.valueOf(order.amount).toString() + " Beverage Napkin");
+		        	writer.newLine();
+		        	writer.write(Integer.valueOf(order.amount).toString() + " Hot Cup");
+		        	writer.newLine();
+		        	writer.write(Integer.valueOf(order.amount).toString() + " Stir Cup");
+		        	writer.newLine();
+		        	writer.write(Integer.valueOf(order.amount).toString() + " 1 drip tray for each container");
+		        	writer.newLine();
+		        	
+		        	
+		            System.out.println(Integer.valueOf(order.amount).toString() + " Beverage Napkin");
+		            System.out.println(Integer.valueOf(order.amount).toString() + " Hot Cup");
+		            System.out.println(Integer.valueOf(order.amount).toString() + " Stir Cup");
+		            System.out.println(Integer.valueOf(order.amount).toString() + " 1 drip tray for each container");
+		        }
+		    }
+		    
+		    writer.close();
+		    
+		    
+		} catch (IOException e) {
+            // Trata exceções de E/S (Input/Output)
+            e.printStackTrace();
+        }
+		
+		return fileName;
 	}
 }
